cc39b10de8bb182dfca5829a8337bc8b5a85c053
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-02-01 18:12:47 +0100

    WIP: Implemented Navigation Drawer

--- frontend/app/src/main/java/com/iesvirgendelcarmen/meetyourplanet/MainActivity.kt
+++ frontend/app/src/main/java/com/iesvirgendelcarmen/meetyourplanet/MainActivity.kt
@@ -2,19 +2,11 @@ package com.iesvirgendelcarmen.meetyourplanet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
-import android.widget.Toast
+
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.iesvirgendelcarmen.meetyourplanet.adapters.SystemRecyclerAdapter
+
 import com.iesvirgendelcarmen.meetyourplanet.fragment.HomeFragment
 import com.iesvirgendelcarmen.meetyourplanet.fragment.SystemsListFragment
-import com.iesvirgendelcarmen.meetyourplanet.model.PlanetarySystem
-import com.iesvirgendelcarmen.meetyourplanet.model.Resource
-import com.iesvirgendelcarmen.meetyourplanet.model.SystemViewModel
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +16,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
 
         if(savedInstanceState == null){
             supportFragmentManager
