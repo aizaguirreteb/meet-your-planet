192ced04a8f03bafbfbabb3d293241b83b3e4ff5
José Guerrero <guerrerogallegojose@outlook.com>
2020-02-02 15:33:53 +0100

    CRUD For Systems

--- backend/src/controller/systems.js
+++ backend/src/controller/systems.js
@@ -1,10 +1,40 @@
 const systemsModel = require('../model/systems')
 
 module.exports = {
-    getSystems : (req, res) => {
+    getSystems: (req, res) => {
         systemsModel.find({}, (err, docs) => {
             if (err) res.status(500).send(err)
             res.json(docs)
+        })
+    },
+    addSystem: (req, res) => {
+        const newSystem = {
+            star: req.body.star,
+            constellation: req.body.constellation,
+            distanceFromEarth: req.body.distanceFromEarth,
+            imageURL: req.body.imageURL
+        }
+        systemsModel.create(newSystem, (err, result) => {
+            if (err) res.status(500).json(err)
+            res.status(201).json(result)
+        })
+    },
+    getSystemById: (req, res) => {
+        systemsModel.findById({ _id: req.params.id }, (err, docs) => {
+            if (err) res.status(500).send(err)
+            res.json(docs)
+        })
+    },
+    updateSystemById: (req, res) => {
+        systemsModel.updateOne({ _id: req.params.id }, req.body, { new: true }, (err, result) => {
+            if (err) res.status(404).json(err)
+            res.status(204).json(result)
+        })
+    },
+    deleteSystemById: (req, res) => {
+        systemsModel.findOneAndDelete({ _id: req.params.id }, (err, result) => {
+            if (err) res.status(404).json(err)
+            res.send(result)
         })
     }
 }
--- backend/src/routes/systems.js
+++ backend/src/routes/systems.js
@@ -2,6 +2,13 @@ const express = require('express')
 const router = express.Router()
 const systemsSchema = require('../controller/systems')
 
-router.get('/systems', systemsSchema.getSystems)
+router.route('/systems')
+      .get(systemsSchema.getSystems)
+      .post(systemsSchema.addSystem)
+
+router.route('/systems/:id')
+      .get(systemsSchema.getSystemById)
+      .put(systemsSchema.updateSystemById)
+      .delete(systemsSchema.deleteSystemById)
 
 module.exports = router
