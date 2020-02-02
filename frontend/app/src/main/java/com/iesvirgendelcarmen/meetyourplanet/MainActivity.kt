8eddbc098431b626002144a4d64d21a888b45371
Alejandra Izaguirre <aleizatebar@gmail.com>
2020-02-02 12:02:22 +0100

    Implemented Navigation Drawer

--- frontend/app/src/main/java/com/iesvirgendelcarmen/meetyourplanet/MainActivity.kt
+++ frontend/app/src/main/java/com/iesvirgendelcarmen/meetyourplanet/MainActivity.kt
@@ -1,28 +1,48 @@
 package com.iesvirgendelcarmen.meetyourplanet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
-
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
 import com.iesvirgendelcarmen.meetyourplanet.fragment.HomeFragment
 import com.iesvirgendelcarmen.meetyourplanet.fragment.SystemsListFragment
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener{
 
     private val listFragment: SystemsListFragment = SystemsListFragment()
     private val homeFragment: HomeFragment = HomeFragment()
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navigationView: NavigationView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+        var abar = supportActionBar
+        if(abar != null){
+            abar.setHomeAsUpIndicator(R.drawable.menu)
+            abar.setDisplayHomeAsUpEnabled(true)
+
+            abar.setTitle("MEET YOUR PLANET")
+        }
+
+        drawerLayout = findViewById(R.id.drawer)
+        navigationView = findViewById(R.id.navigator)
 
 
-
-        if(savedInstanceState == null){
+        navigationView.setNavigationItemSelectedListener(this)
+        if(savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.container, homeFragment)
@@ -30,7 +50,10 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+
     }
+
+
 
 
     private fun changeFragment(fragment: Fragment) {
@@ -41,4 +64,44 @@ class MainActivity : AppCompatActivity() {
             .commit()
     }
 
+    private fun configurarDrawer(navigationView: NavigationView) {
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+            }
+        }
+        return true
+    }
+
+
+
+    override fun onNavigationItemSelected(menuItem: MenuItem): Boolean {
+        when (menuItem.itemId) {
+            R.id.nav_logino -> {
+                Toast.makeText(this, "Login", Toast.LENGTH_SHORT).show()
+            }
+            R.id.nav_systems -> {
+                changeFragment(listFragment)
+            }
+            R.id.nav_home -> {
+                changeFragment(homeFragment)
+            }
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
 }
